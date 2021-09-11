8611d2c950ed1b985785a053425df4e762726674
Aleksander Filek <aleksanderfilek@pop-os.localdomain>
2021-09-11 18:27:50 +0200

    shader funcionality

--- include/H_shader.h
+++ include/H_shader.h
@@ -7,6 +7,10 @@ typedef void* HeroShader;
 extern "C" {
 #endif
 
+extern HeroShader* heroShaderLoad(const char* vertexShader, const char* fragmentShader);
+extern void heroShaderUnload(HeroShader* shader);
+extern void heroShaderBind(const HeroShader* shader);
+
 #ifdef __cplusplus
 }
 #endif
---
+++ source/H_shader.c
@@ -0,0 +1,116 @@
+#include<stdint.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<GL/glew.h>
+#include<GL/gl.h>
+#include<GL/glu.h>
+
+#include"H_debug.h"
+
+typedef struct 
+{
+    const char* vertexShader;
+    const char* fragmentShader;
+
+    uint32_t glID;
+} HeroShader;
+
+HeroShader* heroShaderLoad(const char* vertexShader, const char* fragmentShader)
+{
+    uint32_t program = glCreateProgram();
+    uint32_t vertex = 0, fragment = 0;
+
+    char* buffer = NULL;
+    long length = 0;
+
+    FILE* file = NULL;
+
+    if(vertexShader)
+    {
+        file = fopen(vertexShader, "r");
+
+        if(file == NULL)
+        {
+            printf("Could not open vertex shader file: %s\n", vertexShader);
+            return NULL;
+        }
+
+        fseek (file, 0, SEEK_END);
+        length = ftell(file);
+        fseek (file, 0, SEEK_SET);
+        buffer = (char*)malloc(length);
+        fread(buffer, 1, length, file);
+
+        fclose(file);
+
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, (const char**)&buffer, NULL);
+        glCompileShader(vertex);        
+        DEBUG_CODE( glShaderCheckError(vertex, GL_COMPILE_STATUS); )
+
+        glAttachShader(program, vertex);
+        DEBUG_CODE( glProgramCheckError(program, GL_ATTACHED_SHADERS); )
+
+        free(buffer);
+    }
+
+    if(fragmentShader)
+    {
+        file = fopen(fragmentShader, "r");
+
+        if(file == NULL)
+        {
+            printf("Could not open fragment shader file: %s\n", fragmentShader);
+            return NULL;
+        }
+
+        fseek (file, 0, SEEK_END);
+        length = ftell(file);
+        fseek (file, 0, SEEK_SET);
+        buffer = (char*)malloc(length);
+        fread(buffer, 1, length, file);
+
+        fclose(file);
+
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, (const char**)&buffer, NULL);
+        glCompileShader(fragment);
+        DEBUG_CODE( glShaderCheckError(fragment, GL_COMPILE_STATUS); )
+
+        glAttachShader(program, fragment);
+        DEBUG_CODE( glProgramCheckError(program, GL_ATTACHED_SHADERS); )
+
+        free(buffer);
+    }
+
+    glLinkProgram(program);
+    int  success;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    DEBUG_CODE( glProgramCheckError(program, GL_LINK_STATUS); )
+    if(success == GL_FALSE)
+    {
+        return NULL;
+    }
+
+    glDeleteShader(vertex);
+    DEBUG_CODE( glShaderCheckError(vertex, GL_DELETE_STATUS); )
+    glDeleteShader(fragment);
+    DEBUG_CODE( glShaderCheckError(fragment, GL_DELETE_STATUS); )
+
+    HeroShader* shader = (HeroShader*)malloc(sizeof(HeroShader));
+    *shader = (HeroShader){ vertexShader, fragmentShader, program };
+
+    return shader;
+}
+
+void heroShaderUnload(HeroShader* shader)
+{
+    glDeleteProgram(shader->glID);
+    free(shader);
+}
+
+void heroShaderBind(const HeroShader* shader)
+{
+    glUseProgram(shader->glID);
+    glCheckError();
+}
