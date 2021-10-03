26225f139f9b96d033e343ace84e88a4b28386f9
unknown <filekaleksander@gmail.com>
2021-10-03 21:25:19 +0200

    build clean up

--- build/build.bat
+++ build/build.bat
@@ -1,6 +1,9 @@
 @echo off
-gcc -std=c99 -D DEBUG -c -Iinclude/ source/*.c -fPIC -lm
-gcc -std=c99 -D DEBUG -o libhero.dll *.o -shared -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_mixer -lsoil -lglew32 -lopengl32 -lglu32  -lm
-gcc -std=c99 -D DEBUG -L. -Iinclude/ -o main.exe main.c -lhero -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_mixer -lsoil -lglew32 -lopengl32 -lglu32 -lm
+echo Compiling Hero Library
+gcc -std=c99 -D DEBUG -c -I../include/ ../source/*.c -fPIC -lm
+gcc -std=c99 -D DEBUG -o libhero.dll *.o -shared -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_mixer -lsoil -lglew32 -lopengl32 -lglu32 -lm
+
+echo Compiling Game
+gcc -std=c99 -D DEBUG -L. -I../include/ -o main.exe main.c -lhero -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_mixer -lsoil -lglew32 -lopengl32 -lglu32 -lm
 
 del *.o
--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-gcc -std=c99 -D DEBUG -c -Iinclude/ source/*.c -fPIC -lm
+echo Compiling Hero Library
+gcc -std=c99 -D DEBUG -c -I../include/ ../source/*.c -fPIC -lm
 gcc -std=c99 -D DEBUG -o libhero.so *.o -shared -lm
-gcc -std=c99 -D DEBUG -L. -Iinclude/ -o main.out main.c -lhero -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_mixer -lSOIL -lGLEW -lGL -lGLU -lm
+
+echo Compiling Game
+gcc -std=c99 -D DEBUG -L. -I../include/ -o main.out main.c -lhero -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_mixer -lSOIL -lGLEW -lGL -lGLU -lm
 
 unset LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/home/username/Dokumenty/projekty/HeroCore:$LD_LIBRARY_PATH
