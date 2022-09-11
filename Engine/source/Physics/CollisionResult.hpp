9ce1159d42970ec583d8bf282a5c1cd490c19cf5
Aleksander Filek <filekaleksander@gmail.com>
2022-09-11 20:39:00 +0200

    physics setup

--- Engine/source/Physics/CollisionResult.hpp
+++ Engine/source/Physics/CollisionResult.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+namespace Hero
+{
+
 struct CollisionResult
 {
   bool Hit = false;
 };
+
+}
---
+++ Engine/source/Physics/PhysicsManager.cpp
@@ -0,0 +1,26 @@
+#include"PhysicsManager.hpp"
+
+namespace Hero
+{
+
+PhysicsManager::PhysicsManager(const Hero::Sid& NewId) : Hero::Actor(NewId)
+{
+
+}
+
+void PhysicsManager::Start()
+{
+
+}
+
+void PhysicsManager::Update()
+{
+
+}
+
+void PhysicsManager::End()
+{
+
+}
+
+}
